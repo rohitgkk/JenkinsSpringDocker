58830b2eec801aa580982dfad4bcaf2419dd77a1
rohitgkk <shingalapurr@hcl.com>
2016-12-29 16:47:13 +0530

    First Commit

---
+++ src/main/java/com/TmobileDemo/spring/helloWorld/RootController.java
@@ -0,0 +1,13 @@
+package com.TmobileDemo.spring.helloWorld;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+	@RequestMapping("/")
+	 public String home() {
+		 return "Hello World";
+	 }
+}
