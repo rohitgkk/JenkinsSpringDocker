5a551be84f226c726e81e71c4de5136f43b4d1b3
rohitgkk <shingalapurr@hcl.com>
2016-12-29 17:11:04 +0530

    Fourth Commit

--- src/main/java/com/TmobileDemo/spring/helloWorld/RootController.java
+++ src/main/java/com/TmobileDemo/spring/helloWorld/RootController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class RootController {
 	@RequestMapping("/")
 	 public String home() {
-		 return "Hellokkkks.... World";
+		 return "Hellommmkkkks.... World";
 	 }
 }
